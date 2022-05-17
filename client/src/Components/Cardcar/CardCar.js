05562ea92b8916b3c3d358071bfce87ce3e3f306
hamzabelakri <hamzabelakri92@gmail.com>
2022-05-18 00:36:37 +0100

    step10

--- client/src/Components/Cardcar/CardCar.js
+++ client/src/Components/Cardcar/CardCar.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import {useNavigate} from "react-router-dom"
-import {getOneProduct} from "../../Redux/Actions/ProductAction"
-import {useDispatch} from "react-redux";
-
-
+import { useNavigate } from "react-router-dom";
+import { getOneProduct } from "../../Redux/Actions/ProductAction";
+import { useDispatch } from "react-redux";
 
 function Cardcar({ product }) {
-    const dispatch= useDispatch;
-  const navigate=useNavigate();
-  const handleClick=()=>{
+  const dispatch = useDispatch;
+  const navigate = useNavigate();
+  const handleClick = () => {
     /* dispatch (getOneProduct(product._id, navigate)) */
-   alert (product._id)
-  }
+    alert(product._id);
+  };
   return (
-    <Card style={{ width: "18rem"}}>
-      <Card.Img variant="top" src={product.image} />
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        style={{ width: "100%", height: "15vw", objectFit: "cover" }}
+        src={product.image}
+      />
       <Card.Body>
         <Card.Title>{product.brand}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
-        <Button variant="primary" onClick={handleClick}>More Info</Button>
+        <Button variant="primary" onClick={handleClick}>
+          More Info
+        </Button>
       </Card.Body>
     </Card>
   );
