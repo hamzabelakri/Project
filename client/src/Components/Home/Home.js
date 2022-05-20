3c634996d3137267943a86215d539ce13a1a0d60
hamzabelakri <hamzabelakri92@gmail.com>
2022-05-20 21:22:56 +0100

    step 14

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "./Home.css"
+import { FaCar, FaComment, FaSearch } from "react-icons/fa";
+import { BsFillCreditCardFill } from "react-icons/bs";
+import { AiFillDollarCircle } from "react-icons/ai";
+
+import "./Home.css";
 function Home() {
   return (
     <div>
@@ -11,8 +15,6 @@ function Home() {
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
-          
-        
         </Carousel.Item>
 
         <Carousel.Item>
@@ -40,57 +42,140 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <div class="col d-flex flex-wrap justify-content-center text-uppercase align-items-center">
-            <h1 class="font-weight-bold mx-1">why choose</h1>
-            <h1 className="section-title-special mx-1">DasAuto</h1>
-          </div>
-<p>Here at Autoweb Design, we are specialists in Automotive website design and digital solutions. We work with everyone from independent car dealerships right through to vehicle manufacturers. Our Automotive Website Design and Digital Marketing solutions are designed to convert and as we are a data driven agency, we are constantly improving our platform to ensure car dealers have the very latest in Automotive technology.
 
-We pride ourselves in providing excellent customer service to our customers and aim to build a relationship with each and every one of our customers. We have also developed our own automotive e-commerce solution which will be available to all of our customers as standard. If you are paying a third party for an e-commerce solution on your Automotive website, speak to Autoweb Design and see how you can bring it all under one roof.</p>
-<p>"We know from feedback given by customers, that the website is easy to navigate and easy to get the information you require when looking to buy a car".</p>
- 
+      <div
+        id="section"
+        class="col d-flex flex-wrap justify-content-center text-uppercase align-items-center"
+      >
+        <h1 class="font-weight-bold mx-1">why choose</h1>
+        <h1 className="section-title-special mx-1">DasAuto</h1>
+      </div>
+      <p>
+        Here at Autoweb Design, we are specialists in Automotive website design
+        and digital solutions. We work with everyone from independent car
+        dealerships right through to vehicle manufacturers. Our Automotive
+        Website Design and Digital Marketing solutions are designed to convert
+        and as we are a data driven agency, we are constantly improving our
+        platform to ensure car dealers have the very latest in Automotive
+        technology. We pride ourselves in providing excellent customer service
+        to our customers and aim to build a relationship with each and every one
+        of our customers. We have also developed our own automotive e-commerce
+        solution which will be available to all of our customers as standard. If
+        you are paying a third party for an e-commerce solution on your
+        Automotive website, speak to Autoweb Design and see how you can bring it
+        all under one roof.
+      </p>
+      <p>
+        "We know from feedback given by customers, that the website is easy to
+        navigate and easy to get the information you require when looking to buy
+        a car".
+      </p>
 
- <div className="container">
-<div className="one" class="col-sm-6 col-lg-3 text-center my-3">
-            <a href="" class="skills-icon rounded-circle p-2">
-              <i class="fas fa-car fa-fw"></i>
-            </a>
-            <h6 class="text-uppercase font-weight-bold my-3">all brands</h6>
-            <div class="skills-underline"></div>
-            <p class="w-75 mx-auto text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum.</p>
+      <div className="container" style={{ marginTop: "40px", marginBottom: "20px"}}>
+        <div id="one" class="col-sm-6 col-lg-3 text-center my-3">
+          <a href="/#" id="icoon" class="question-icon mr-3">
+            <i class="fas fa-dollar-sign"></i>
+            <FaCar />
+          </a>
+          <h6 id="subtitle" class="text-uppercase font-weight-bold my-3">
+            all brands
+          </h6>
+
+          <div class="skills-underline"></div>
+          <p class="w-75 mx-auto text-muted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            interdum.
+          </p>
+        </div>
+      
+        <div id="one" class="col-sm-6 col-lg-3 text-center my-3">
+        <a id="icoon">
+          <FaComment />
+          </a>
+          <h6 id="subtitle" class="text-uppercase font-weight-bold my-3">
+            free support
+          </h6>
+          <div class="skills-underline"></div>
+          <p class="w-75 mx-auto text-muted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            interdum.
+          </p>
+        </div>
+
+        <div id="one" class="col-sm-6 col-lg-3 text-center my-3">
+        <a id="icoon">
+          <BsFillCreditCardFill />
+          </a>
+          <h6 id="subtitle" class="text-uppercase font-weight-bold my-3">
+            caring
+          </h6>
+          <div class="skills-underline"></div>
+          <p class="w-75 mx-auto text-muted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            interdum.
+          </p>
+        </div>
+
+        <div id="one" class="col-sm-6 col-lg-3 text-center my-3">
+          <a id="icoon">
+            <BsFillCreditCardFill />
+          </a>
+          <h6 id="subtitle" class="text-uppercase font-weight-bold my-3">
+            affordable
+          </h6>
+          <div class="skills-underline"></div>
+          <p class="w-75 mx-auto text-muted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            interdum.
+          </p>
+        </div>
+      </div>
+
+      <section class="question py-5" id="question">
+        <div class="container">
+          <div class="row">
+            <div class="col-10 mx-auto col-md-6 d-flex justify-content-between p-4 my-2 question-grey">
+              <a href="/#" id="icon" class="question-icon mr-3">
+                <i class="fas fa-search"></i>
+                <FaSearch />
+              </a>
+
+              <div class="question-text w-75">
+                <h4
+                  id="reference"
+                  class="question-title text-capitalize font-weight-bold"
+                >
+                  are you looking for a car?
+                </h4>
+                <p id="reference1" class="question-info">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  interdum.
+                </p>
+              </div>
+            </div>
+
+            <div class="col-10 mx-auto col-md-6 d-flex justify-content-between p-4 my-2 question-yellow">
+              <a href="/#" id="icon" class="question-icon mr-3">
+                <i class="fas fa-dollar-sign"></i>
+                <AiFillDollarCircle />
+              </a>
+
+              <div class="question-text w-75">
+                <h4
+                  id="reference"
+                  class="question-title text-capitalize font-weight-bold"
+                >
+                  do you want to sell a car?
+                </h4>
+                <p id="reference1" class="question-info">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  interdum.
+                </p>
+              </div>
+            </div>
           </div>
-          
-          <div className="one" class="col-sm-6 col-lg-3 text-center my-3">
-            <a href="" class="skills-icon rounded-circle p-2">
-              <i class="fas fa-comment fa-fw"></i>
-            </a>
-            <h6 class="text-uppercase font-weight-bold my-3">free support</h6>
-            <div class="skills-underline"></div>
-            <p class="w-75 mx-auto text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum.</p>
-          </div>
-          
-          <div className="one" class="col-sm-6 col-lg-3 text-center my-3">
-            <a href="" class="skills-icon rounded-circle p-2">
-              <i class="fas fa-people-carry fa-fw"></i>
-            </a>
-            <h6 class="text-uppercase font-weight-bold my-3">caring</h6>
-            <div class="skills-underline"></div>
-            <p class="w-75 mx-auto text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum.</p>
-          </div>
-          
-          <div className="one" class="col-sm-6 col-lg-3 text-center my-3">
-            <a href="" class="skills-icon rounded-circle p-2">
-              <i class="fas fa-wallet fa-fw"></i>
-            </a>
-            <h6 class="text-uppercase font-weight-bold my-3">affordable</h6>
-            <div class="skills-underline"></div>
-            <p class="w-75 mx-auto text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum.</p>
-          </div>
-       </div> 
- 
- 
- 
- 
+        </div>
+      </section>
     </div>
   );
 }
